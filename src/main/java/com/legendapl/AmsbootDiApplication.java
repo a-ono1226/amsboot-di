39148daad313f04bf664e2e5c4d358d94366508b
Atsushi Ono <a-ono@legendapl.com>
2016-11-01 15:35:37 +0900

    first commit

---
+++ src/main/java/com/legendapl/AmsbootDiApplication.java
@@ -0,0 +1,12 @@
+package com.legendapl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsbootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsbootDiApplication.class, args);
+	}
+}
