7fb80dadc3698d5f43e13d9d10de1c8af81071fd
Atsushi Ono <a-ono@legendapl.com>
2016-11-01 20:29:54 +0900

    Use CommandLineRunner.

--- src/main/java/com/legendapl/AmsbootDiApplication.java
+++ src/main/java/com/legendapl/AmsbootDiApplication.java
@@ -1,19 +1,32 @@
 package com.legendapl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.legendapl.app.Frontend;
+import com.legendapl.app.Argument;
+import com.legendapl.app.ArgumentResolver;
+import com.legendapl.app.Calculator;
 
-@EnableAutoConfiguration
-@ComponentScan
-public class AmsbootDiApplication {
+//@SpringBootApplicationに各アノテーションが内包されている
+@SpringBootApplication
+public class AmsbootDiApplication implements CommandLineRunner {
+	// ComandLineRunnderはコマンドラインアプリケーション用のインタフェース
+	@Autowired
+	ArgumentResolver argres;
+	@Autowired
+	Calculator calc;
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(AmsbootDiApplication.class, args);
-		Frontend frontend = context.getBean(Frontend.class);
-		frontend.run();
+		SpringApplication.run(AmsbootDiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Frontendクラスの実行処理をこちらに移す
+		System.out.println("Enter 2 numbers like 'a b' : ");
+		Argument argsIn = argres.resolve(System.in);
+		System.out.println("result = " + calc.calc(argsIn.getA(), argsIn.getB()));
 	}
 }
